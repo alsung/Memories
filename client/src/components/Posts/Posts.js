a08d36fce7156c44c9962ae0207147c3f61d57ab
Alex Sung <alexsung98@gmail.com>
2022-01-07 19:39:59 -0800

    import styles for posts and post components, functionality not implemented

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post'
 
+import useStyles from './styles';
+
+// fetch data from global redux store with selectors
+
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+
+    console.log(posts);
+
     return (
         <>
             <h1>POSTS</h1>
